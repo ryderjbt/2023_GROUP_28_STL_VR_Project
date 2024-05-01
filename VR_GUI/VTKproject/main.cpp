189dd48be76491db1b229f33a961d171ace4fd8c
Kieran Redding (Student) <eeykr3@nottingham.ac.uk>
2024-05-01 11:35:42 +0100

    Adaptations made to files for VR Render

--- VR_GUI/VTKproject/main.cpp
+++ VR_GUI/VTKproject/main.cpp
@@ -1,5 +1,4 @@
 #include "mainwindow.h"
-
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
@@ -8,16 +7,26 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    // Initialize translator for internationalization
     QTranslator translator;
+
+    // Get the system's preferred UI languages
     const QStringList uiLanguages = QLocale::system().uiLanguages();
+
+    // Iterate through the preferred UI languages to find the appropriate translation file
     for (const QString &locale : uiLanguages) {
         const QString baseName = "VTKproject_" + QLocale(locale).name();
         if (translator.load(":/i18n/" + baseName)) {
+            // If translation file is found, install the translator and break the loop
             a.installTranslator(&translator);
             break;
         }
     }
+
+    // Create and show the main window
     MainWindow w;
     w.show();
+
+    // Start the application event loop
     return a.exec();
 }
